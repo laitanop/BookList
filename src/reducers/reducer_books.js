d8fec0e43fc710f3d0dd2d1bb82d55f775351395
laitanop <laitanop@yahoo.com>
2016-10-18 09:23:45 -0600

    adding, .babelrc, .gitignore, index.html, package.jason, src/components/containers/reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,16 @@
+// Function that return an Array of books. 
+
+export default function() {
+	return [
+		{ title: 'The Girls'},
+		{ title: 'My name is Lucy Barton' },
+		{ title: 'Homegoing'},
+		{ title: 'When Breath Becomes Air' } 
+
+
+
+	]
+
+	
+
+}
