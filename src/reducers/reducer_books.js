f56b7536720ee10da7a8517e3f3b1437ea461464
laitanop <laitanop@yahoo.com>
2016-10-18 10:22:50 -0600

    Modified

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,9 @@
+
 // Function that return an Array of books. 
 
 export default function() {
 	return [
-		{ title: 'The Girls'},
+		{ title: 'Evicted'},
 		{ title: 'My name is Lucy Barton' },
 		{ title: 'Homegoing'},
 		{ title: 'When Breath Becomes Air' } 
